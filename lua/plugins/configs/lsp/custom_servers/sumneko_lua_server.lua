7d6df08b228e3dbe1c78dd15033e3fb8306864ef
Ghasak <mdotgh@gmail.com>
2023-01-06 18:20:12 +0900

    [macbook13 - work] develop the env for my daily work

--- lua/plugins/configs/lsp/custom_servers/sumneko_lua_server.lua
+++ lua/plugins/configs/lsp/custom_servers/sumneko_lua_server.lua
@@ -1,28 +1,50 @@
 local runtime_path = vim.split("package.path", ";")
 table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
-
+--
+-- return {
+--   settings = {
+--     Lua = {
+--       runtime = {
+--         -- LuaJIT inn the case of Neovim
+--         version = "LuaJIT",
+--         path = vim.split(package.path, ";")
+--       },
+--       diagnostics = { globals = { "vim", "use", "packer_plugins" } },
+--       workspace = {
+--         library = {
+--           checkThirdParty = false, -- THIS IS THE IMPORTANT LINE TO ADD
+--           [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+--           [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+--           vim.api.nvim_get_runtime_file("", true),
+--         },
+--         maxPreload = 100000,
+--         preloadFileSize = 10000
+--       },
+--       telemetry = { enable = false }
+--     }
+--   }
+--
+--
+-- }
 return {
+
   settings = {
     Lua = {
       runtime = {
-        -- LuaJIT inn the case of Neovim
-        version = "LuaJIT",
-        path = vim.split(package.path, ";")
+        version = 'LuaJIT',
+        path = runtime_path,
       },
-      diagnostics = { globals = { "vim", "use", "packer_plugins" } },
+      diagnostics = {
+        globals = { 'vim' },
+      },
       workspace = {
-        library = {
-          [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-          [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
-          vim.api.nvim_get_runtime_file("", true),
-        },
-        maxPreload = 100000,
-        preloadFileSize = 10000
+        library = vim.api.nvim_get_runtime_file('', true),
+        checkThirdParty = false, -- THIS IS THE IMPORTANT LINE TO ADD
       },
-      telemetry = { enable = false }
-    }
-  }
-
-
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
 }
