297e2183aeee4fe3d3757b91691c1c665cb92e29
Ghasak <mdotgh@gmail.com>
2023-04-16 15:40:25 +0900

    [refactoring v.0.0.3] cleaning some code and optimizing startup time..

--- lua/core/utils.lua
+++ lua/core/utils.lua
@@ -2,18 +2,7 @@ local M = {}
 local merge_tb = vim.tbl_deep_extend
 
 M.load_config = function()
-  local config = require "core.default_config"
-  local chadrc_path = vim.api.nvim_get_runtime_file("lua/custom/chadrc.lua", false)[1]
-
-  if chadrc_path then
-    local chadrc = dofile(chadrc_path)
-
-    config.mappings = M.remove_disabled_keys(chadrc.mappings, require "core.mappings")
-    config = merge_tb("force", config, chadrc)
-  end
-
-  config.mappings.disabled = nil
-  return config
+  config =  {}
 end
 
 M.remove_disabled_keys = function(chadrc_mappings, default_mappings)
@@ -73,16 +62,7 @@ M.load_mappings = function(section, mapping_opt)
         end
       end
     end
-
-    local mappings = require("core.utils").load_config().mappings
-
     if type(section) == "string" then
-      --mappings[section]["plugin"] = nil
-      mappings = { mappings[section] }
-    end
-
-    for _, sect in pairs(mappings) do
-      set_section_map(sect)
     end
   end)
 end
