7398db311bb3983016e24d9504026e5f3f0ec37f
Ghasak <mdotgh@gmail.com>
2024-02-27 02:04:18 +0900

    [update] neovim now is in final form

--- lua/plugins/configs/myGenAI.lua
+++ lua/plugins/configs/myGenAI.lua
@@ -14,6 +14,7 @@ M.config = function()
     replace = true,
   }
   gen.model = 'zephyr:latest'
+  --gen.model = 'gemma:7b'
 end
 
 return M
--- lua/plugins/pluginsHub.lua
+++ lua/plugins/pluginsHub.lua
@@ -14,63 +14,63 @@ return {
     end,
   },
 
-  {
-    "xiyaowong/transparent.nvim",
-    cmd = {
-      "TransparentDisabl",
-      "TransparentToggle",
-      "TransparentEnable",
-    },
-    config = function()
-      -- This is just a test
-      require("transparent").setup { -- Optional, you don't have to run setup.
-        groups = { -- table: default groups
-          "Normal",
-          "NormalNC",
-          "Comment",
-          "Constant",
-          "Special",
-          "Identifier",
-          "Statement",
-          "PreProc",
-          "Type",
-          "Underlined",
-          "Todo",
-          "String",
-          "Function",
-          "Conditional",
-          "Repeat",
-          "Operator",
-          "Structure",
-          "LineNr",
-          "NonText",
-          "SignColumn",
-          "CursorLine",
-          "CursorLineNr",
-          "StatusLine",
-          "StatusLineNC",
-          "EndOfBuffer",
-        },
-        extra_groups = {
-          "NormalFloat", -- plugins which have float panel such as Lazy, Mason, LspInfo
-          "NvimTreeNormal", -- NvimTree
-          "NvimTreeNC",
-          "NvimTreeEndOfBuffer",
-          "IndentBlanklineContext",
-          "IndentBlanklineContextChar",
-          "IndentBlanklineContext",
-          "IndentBlanklineContextSpaceChar",
-          "IndentBlanklineIndent1",
-          "IndentBlanklineIndent2",
-          "IndentBlanklineIndent3",
-          "IndentBlanklineIndent4",
-          "IblIndent",
-
-        }, -- table: additional groups that should be cleared
-        exclude_groups = {}, -- table: groups you don't want to clear
-      }
-    end,
-  },
+  -- {
+  --   "xiyaowong/transparent.nvim",
+  --   cmd = {
+  --     "TransparentDisabl",
+  --     "TransparentToggle",
+  --     "TransparentEnable",
+  --   },
+  --   config = function()
+  --     -- This is just a test
+  --     require("transparent").setup { -- Optional, you don't have to run setup.
+  --       groups = { -- table: default groups
+  --         "Normal",
+  --         "NormalNC",
+  --         "Comment",
+  --         "Constant",
+  --         "Special",
+  --         "Identifier",
+  --         "Statement",
+  --         "PreProc",
+  --         "Type",
+  --         "Underlined",
+  --         "Todo",
+  --         "String",
+  --         "Function",
+  --         "Conditional",
+  --         "Repeat",
+  --         "Operator",
+  --         "Structure",
+  --         "LineNr",
+  --         "NonText",
+  --         "SignColumn",
+  --         "CursorLine",
+  --         "CursorLineNr",
+  --         "StatusLine",
+  --         "StatusLineNC",
+  --         "EndOfBuffer",
+  --       },
+  --       extra_groups = {
+  --         "NormalFloat", -- plugins which have float panel such as Lazy, Mason, LspInfo
+  --         "NvimTreeNormal", -- NvimTree
+  --         "NvimTreeNC",
+  --         "NvimTreeEndOfBuffer",
+  --         "IndentBlanklineContext",
+  --         "IndentBlanklineContextChar",
+  --         "IndentBlanklineContext",
+  --         "IndentBlanklineContextSpaceChar",
+  --         "IndentBlanklineIndent1",
+  --         "IndentBlanklineIndent2",
+  --         "IndentBlanklineIndent3",
+  --         "IndentBlanklineIndent4",
+  --         "IblIndent",
+  --
+  --       }, -- table: additional groups that should be cleared
+  --       exclude_groups = {}, -- table: groups you don't want to clear
+  --     }
+  --   end,
+  -- },
 
   -- ==========================================================================
   -- 	                      Utilities for NVIM IDE Env
