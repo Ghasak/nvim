0be5688fc45ef0d664bac4d3945c28446a77b19f
Ghasak <mdotgh@gmail.com>
2024-07-08 23:07:45 +0900

    [hotfix] adding some features for the nvim v0.10

--- lua/plugins/configs/myGit.lua
+++ lua/plugins/configs/myGit.lua
@@ -3,21 +3,44 @@ if not status_ok then
   return
 end
 
+-- Set up the highlight groups
+vim.api.nvim_set_hl(0, 'GitSignsAdd', { fg = '#048BA8', bg = '#2c333c' })
+vim.api.nvim_set_hl(0, 'GitSignsAddLn', { fg = '#048BA8', bg = '#2c333c' })
+vim.api.nvim_set_hl(0, 'GitSignsAddNr', { fg = '#048BA8', bg = '#2c333c' })
+
+vim.api.nvim_set_hl(0, 'GitSignsChange', { fg = '#ffff00', bg = '#2c333c' })
+vim.api.nvim_set_hl(0, 'GitSignsChangeLn', { fg = '#ffff00', bg = '#2c333c' })
+vim.api.nvim_set_hl(0, 'GitSignsChangeNr', { fg = '#ffff00', bg = '#2c333c' })
+
+vim.api.nvim_set_hl(0, 'GitSignsChangedelete', { fg = '#ff4500', bg = '#2c333c' })
+vim.api.nvim_set_hl(0, 'GitSignsChangedeleteLn', { fg = '#ff4500', bg = '#2c333c' })
+vim.api.nvim_set_hl(0, 'GitSignsChangedeleteNr', { fg = '#ff4500', bg = '#2c333c' })
+
+vim.api.nvim_set_hl(0, 'GitSignsDelete', { fg = '#ff0000', bg = '#2c333c' })
+vim.api.nvim_set_hl(0, 'GitSignsDeleteLn', { fg = '#ff0000', bg = '#2c333c' })
+vim.api.nvim_set_hl(0, 'GitSignsDeleteNr', { fg = '#ff0000', bg = '#2c333c' })
+
+vim.api.nvim_set_hl(0, 'GitSignsTopdelete', { fg = '#ff6347', bg = '#2c333c' })
+vim.api.nvim_set_hl(0, 'GitSignsTopdeleteLn', { fg = '#ff6347', bg = '#2c333c' })
+vim.api.nvim_set_hl(0, 'GitSignsTopdeleteNr', { fg = '#ff6347', bg = '#2c333c' })
+
+-- Set up gitsigns
 gitsigns.setup {
   signs = {
-    add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-    --add = { hl = "GitSignsAdd", text = " ", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-    change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-    --change = { hl = "GitSignsChange", text = " ", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-    --delete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-    --topdelete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-    delete = { hl = "GitSignsDelete", text = "󰇝", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-    topdelete = { hl = "GitSignsDelete", text = "󰇝", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-    changedelete = { hl = "GitSignsChange", text = "󰇝", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-    --changedelete = { hl = "GitSignsChange", text = "李", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+    -- deprecated v.10
+    -- add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+    -- change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+    -- delete = { hl = "GitSignsDelete", text = "󰇝", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+    -- topdelete = { hl = "GitSignsDelete", text = "󰇝", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+    -- changedelete = { hl = "GitSignsChangedelete", text = "󰇝", numhl = "GitSignsChangedeleteNr", linehl = "GitSignsChangedeleteLn" },
+    add = {text = "▎"},
+    change = { text = "▎"},
+    delete = { text = "󰇝"},
+    topdelete = { text = "󰇝"},
+    changedelete = {  text = "󰇝"},
   },
   signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-  numhl = false, -- Toggle with `Gitsigns toggle_numhl`
+  numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
   linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
   word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
   watch_gitdir = {
@@ -51,3 +74,4 @@ gitsigns.setup {
     enable = false,
   },
 }
+
