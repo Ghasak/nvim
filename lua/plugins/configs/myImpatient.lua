61d34e236cb14c3477fda70fc8772e1552aca53a
Ghasak <mdotgh@gmail.com>
2025-04-19 14:57:01 +0900

    [dev/core] backbone for our current development nvim v.11

--- lua/plugins/configs/myImpatient.lua
+++
@@ -1,8 +0,0 @@
-local M = {}
-M.setup = function()
-  local impatient_loaded_success = pcall(require, "impatient")
-  if impatient_loaded_success then
-    require("impatient").enable_profile()
-  end
-end
-return M
