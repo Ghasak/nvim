a1e492d01ecf4e666e8979a7d844ba60851845ca
Ghasak <mdotgh@gmail.com>
2025-06-01 09:42:07 +0900

    [release/deploy-v0.1.4] Added obsidian integration plugin - up and running

--- lua/plugins/pluginsHub.lua
+++ lua/plugins/pluginsHub.lua
@@ -1045,6 +1045,7 @@ return {
   -- markdown zettelkasten / wiki and mixing it with a journal, based on telescope.nvim.
   {
     "renerocksai/telekasten.nvim",
+    cond = false,
     event = "InsertEnter",
     cmd = { "Telekasten" },
     dependencies = {
@@ -1055,6 +1056,39 @@ return {
       },
     },
     config = function() require("plugins.configs.p20_myTelekasten").config() end,
+  },
+
+  -- Obsidian.nvim for navigating obsidian notes - intead of telekasten.nvim
+
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*", -- recommended, use latest release instead of latest commit
+    lazy = true,
+    ft = "markdown",
+    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    -- event = {
+    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+    --   -- refer to `:h file-pattern` for more examples
+    --   "BufReadPre path/to/my-vault/*.md",
+    --   "BufNewFile path/to/my-vault/*.md",
+    -- },
+    dependencies = {
+      -- Required.
+      "nvim-lua/plenary.nvim",
+
+      -- see below for full list of optional dependencies 👇
+    },
+    opts = {
+      workspaces = {
+        {
+          name = "personal",
+          path = "~/Documents/myObsidianDoc",
+        },
+      },
+
+      -- see below for full list of options 👇
+    },
   },
 
   {
